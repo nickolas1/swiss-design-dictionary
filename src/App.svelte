d1ce89f84ebb9ed3477437cbfe84a5d8fb773e10
Nick <nick@Nickolass-iMac.domain>
2019-10-14 07:50:05 -0500

    messing around with stock template

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Test from './Test.svelte';
+	import Nested from './Nested.svelte';
+	export let name;
+</script>
+
+<style type="text/scss">
+	$title-color: black;
+	h1 {
+		color: $title-color;
+		font-size: 4em;
+		margin: 0.1em 0;
+	}
+	.poster-vessel {
+		width: 80vw;
+	}
+	.title-bar {
+		width: 100%;
+		height: 3vh;
+		background: $title-color;
+	}
+</style>
+
+<div class='poster-vessel'>
+	<div class='title-bar'></div>
+	<h1>testwort</h1>
+	<div class='title-bar'></div>
+</div>
+<h1>Hello {name}!</h1>
+<Test/>
+<Nested/>
